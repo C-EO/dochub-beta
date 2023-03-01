ddbf5eb7125fe0e2494b451b44df18d813b46728
Dennis Sebalemba <dennis.sebalemba@gmail.com>
2023-03-01 20:30:11 +0300

    Update and rename next.config.mjs to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-import nextra from 'nextra'
-
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',
   staticImage: true,
   defaultShowCopyCode: true,
 })
+
+module.exports = withNextra()
