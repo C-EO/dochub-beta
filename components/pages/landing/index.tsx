827444ba76323de4d126182177642efcfa69e1b1
Dennis Sebalemba <dennis.sebalemba@gmail.com>
2023-03-18 21:30:05 +0300

    Update index.tsx

--- components/pages/landing/index.tsx
+++ components/pages/landing/index.tsx
@@ -137,10 +137,10 @@ function SiteCards() {
           className="turbopackCardBg"
         >
           <div className="absolute top-3 left-3">
-            <CardBadge>alpha</CardBadge>
+            <CardBadge>beta</CardBadge>
           </div>
           <p className="text-lg !w-[280px] md:!w-[340px] font-space-grotesk text-center opacity-50 dark:opacity-70 ">
-            The Nitehub integration for Vercel.
+            A fast, light and reliable integration for Vercel made by Nitehub
           </p>
         </Card>
       </FadeIn>
