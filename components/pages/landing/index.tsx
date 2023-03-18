7d742438832f6ad6200cacd591dbd1ca01ad8c19
Dennis Sebalemba <dennis.sebalemba@gmail.com>
2023-03-18 21:02:04 +0300

    Update index.tsx

--- components/pages/landing/index.tsx
+++ components/pages/landing/index.tsx
@@ -180,7 +180,7 @@ function LandingPage() {
         <Background />
         <FadeIn className="z-10 flex flex-col items-center justify-center w-full h-full">
           <h1 className="mt-12 lg:!mt-20 mx-6 w-[300px] md:!w-full font-extrabold text-5xl lg:text-6xl leading-tight text-center mb-4 bg-clip-text text-transparent bg-gradient-to-b from-black/80 to-black dark:from-white dark:to-[#AAAAAA]">
-            Lightweight Fast Reliable
+            LIGHTWEIGHT FAST RELIABLE
           </h1>
           <p className="mx-6 text-xl max-h-[112px] md:max-h-[96px] w-[315px] md:w-[660px] md:text-2xl font-space-grotesk text-center text-[#666666] dark:text-[#888888]">
             Nitehub is an lightweight, fast, and reliable platform developed by Nitehub Org. for the OSS community.
