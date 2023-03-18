2014a7ecd2940fe69701a082a80a25321c266518
Dennis Sebalemba <dennis.sebalemba@gmail.com>
2023-03-18 20:25:14 +0300

    Update `index.tsx`

--- components/pages/landing/index.tsx
+++ components/pages/landing/index.tsx
@@ -140,7 +140,7 @@ function SiteCards() {
             <CardBadge>alpha</CardBadge>
           </div>
           <p className="text-lg !w-[280px] md:!w-[340px] font-space-grotesk text-center opacity-50 dark:opacity-70 ">
-            Introducing the Rust-powered successor to Webpack.
+            The Nitehub integration for Vercel.
           </p>
         </Card>
       </FadeIn>
@@ -180,11 +180,10 @@ function LandingPage() {
         <Background />
         <FadeIn className="z-10 flex flex-col items-center justify-center w-full h-full">
           <h1 className="mt-12 lg:!mt-20 mx-6 w-[300px] md:!w-full font-extrabold text-5xl lg:text-6xl leading-tight text-center mb-4 bg-clip-text text-transparent bg-gradient-to-b from-black/80 to-black dark:from-white dark:to-[#AAAAAA]">
-            Make Ship Happen
+            Lightweight Fast Reliable
           </h1>
           <p className="mx-6 text-xl max-h-[112px] md:max-h-[96px] w-[315px] md:w-[660px] md:text-2xl font-space-grotesk text-center text-[#666666] dark:text-[#888888]">
-            Turbo is an incremental bundler and build system optimized for
-            JavaScript and TypeScript, written in Rust.
+            Nitehub is an lightweight, fast, and reliable platform developed by Nitehub Org. for the OSS community.
           </p>
         </FadeIn>
         <SiteCards />
